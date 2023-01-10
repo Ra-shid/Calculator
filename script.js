977b3c917240e7e000b491c3e51b002161a2591f
Rashid <65037094+Ra-shid@users.noreply.github.com>
2023-01-11 05:07:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 
-   function enterValue(n){
-        document.querySelector('.window').value = n;
+   function insertValue(n){
+        document.querySelector('.window').value += n;
+        document.querySelector("[name = 'All Clear']").value = 'CE';
 
+    }
+    function equal(){
+        var exp = document.querySelector('.window').value;
+        if(exp){
+            document.querySelector('.window').value = eval(exp)
+        }
+    }
+    function clearAll(){
+        var x = document.querySelector("[name = 'All Clear']").value
+        if(x == 'AC'){
+            document.querySelector('.window').value = " "
+        }
+        else {
+            var exp = document.querySelector('.window').value;
+            document.querySelector('.window').value = exp.substring(0, exp.length - 1);
+        }
+       
     }
